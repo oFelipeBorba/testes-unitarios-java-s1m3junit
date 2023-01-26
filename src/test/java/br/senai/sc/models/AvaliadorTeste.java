0a6daaa929363e359d4589cdeb98ff658de71a25
Felipe Borba <108702072+oFelipeBorba@users.noreply.github.com>
2023-01-26 19:33:19 -0300

    add 1st simple test

---
+++ src/test/java/br/senai/sc/models/AvaliadorTeste.java
@@ -0,0 +1,25 @@
+package br.senai.sc.models;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Date;
+
+public class AvaliadorTeste {
+
+    @Test
+    public void deveEncontrarMaiorEMenorLance(){
+        Usuario brayam = new Usuario("Brayam",25,"111.222.333-45");
+        Lance l1 = new Lance(200,brayam);
+        Lance l2 = new Lance(300,brayam);
+        Lance l3 = new Lance(400,brayam);
+        Leilao leilao =  new Leilao(new Date(2023,01,18),new Date(2023,01,20));
+        leilao.adiciona(l1);
+        leilao.adiciona(l2);
+        leilao.adiciona(l3);
+        Avaliador leiloeiro = new Avaliador();
+        Assert.assertEquals(400,leiloeiro.getMaiorDeTodos(),1);
+        Assert.assertEquals(200,leiloeiro.getMenorDeTodos(),1);
+    }
+
+}
